c93dd6f85cbd70952026d56268756859245e329a
Resource-04 <resource4.code4bharat@gmail.com>
2025-04-05 15:21:21 +0530

    experttoexpertsession controller

--- model/experttoexpertsession.model.js
+++ model/experttoexpertsession.model.js
@@ -1,13 +1,18 @@
 import mongoose, { Schema } from 'mongoose';
 
 const experttoexpertsessionSchema = new Schema({
-    expertId:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:false,
+  consultingExpertID:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+    ref:'Expert'
+  },
+  
+  expertId:{
+      type:mongoose.Schema.Types.ObjectId,
+        required:true,
         ref:'Expert',
     },
-   
-      category: {
+       category: {
         type: String,
         required: true, // Category for the appointment
       },
@@ -26,7 +31,7 @@ const experttoexpertsessionSchema = new Schema({
       },
       duration: {
         type: String,
-        enum: ['Quick-15min', 'Regular-30 min', 'Extra-45 min', 'All Access-60 min'],
+        enum: ['Quick-15 min', 'Regular-30 min', 'Extra-45 min', 'All Access-60 min'],
         required: true, // Duration of the appointment
       },
       optionalNote: {
