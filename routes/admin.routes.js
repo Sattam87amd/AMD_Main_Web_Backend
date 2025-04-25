ebe44592b5b5eccc8c6c2180886113af9ed25793
Resource-5 <resource5.code4bharat@gmail.com>
2025-04-25 12:22:08 +0530

    -change the getbookingdetail function to admincontroller and route too

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,12 +1,15 @@
 import express from 'express';
 
 import { Router } from 'express';
-import { loginAdmin, updateExpertStatus } from '../controller/admin.controller.js';
+import { loginAdmin, updateExpertStatus, getBookingDetails ,getreviwew} from '../controller/admin.controller.js';
 
 
 
 const router = Router();
 
-router.post('/login',loginAdmin )
+router.post('/login', loginAdmin)
 router.put('/experts/:expertId/status', updateExpertStatus);
+router.get("/bookings", getBookingDetails);
+router.get("/review", getreviwew);
+
 export default router;
